bf218c32f5517bdec9fe2267651ad65b6d3527c4
Ariasjuan <juanariasjuan231@gmail.com>
2023-11-28 18:29:04 -0600

    api terminada

--- lib/network/popular_api.dart
+++ lib/network/popular_api.dart
@@ -5,7 +5,7 @@ import 'package:topicos/modelos/popular_model.dart';
 
 class PopularApi {
 
-  final url = Uri.parse('https://api.themoviedb.org/3/movie/popular?api_key=%3Capikey%3E&language=es-MX&page=1');
+  final url = Uri.parse('https://api.themoviedb.org/3/movie/popular?api_key=5019e68de7bc112f4e4337a500b96c56&language=es-MX&page=1');
 
   Future<List<PopularModel>?> getAllPopular() async { //java trabaja con 2, apps moviles con 1
     var res = await http.get(url); //cual es el codigo de error 418 (i'm a teapot)
